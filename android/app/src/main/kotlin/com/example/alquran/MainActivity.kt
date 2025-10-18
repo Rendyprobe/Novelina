c06b564efa91c08706182e74de188b111da23f94
Rendyprobe <rendy221205@gmail.com>
2025-10-18 16:31:28 +0700

    Fitur Komentar dan Foto Profil

--- android/app/src/main/kotlin/com/example/alquran/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.novelina
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/core/app_constants.dart
+++
@@ -1,9 +0,0 @@
-
-class AppConstants {
-  static const String appName = 'Novelina';
-  static const String developerName = 'Dibuat oleh: [RendyProbe]';
-  static const String welcomeMessage = 'Selamat datang di dunia novel';
-  static const String loginSuccessMessage = 'Login berhasil!';
-  static const String registerSuccessMessage = 'Selamat kamu telah mendaftar, silakan login.';
-  static const String logoAsset = 'assets/images/Logo_Novelina.jpg';
-}
